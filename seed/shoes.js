eacdeccff1c848dd72ea830806fe46ed8dc42557
rcraig <ryanpcraig1@gmail.com>
2021-08-26 13:25:53 -0500

    populated seed files, updated the models, and did some db testing

--- seed/shoes.js
+++ seed/shoes.js
@@ -8,32 +8,38 @@ const main = async () => {
     {
       brand: 'Nike',
       model: 'Air Jordan',
-      color: 'Red'
+      color: 'Red',
+      image: './images/nike-air-jordan-red.jpg'
     },
     {
       brand: 'Adidas',
-      model: 'Yeezy Boost 350',
-      color: 'Cream V2'
+      model: 'Yeezy Boost 350 V2',
+      color: 'Cream',
+      image: './images/Adidas-Yeezy-350-V2-Cream.jpg'
+    },
+    {
+      brand: 'New Balance',
+      model: '997',
+      color: 'Grey',
+      image: './new_balance_997_grey.jpg'
     },
     {
       brand: 'Adidas',
-      model: 'Yeezy Boost 350',
-      color: 'Cream V2'
+      model: 'NMD',
+      color: 'Black',
+      image: './images/adidas-nmd-black.jpg'
     },
     {
-      brand: 'Adidas',
-      model: 'Yeezy Boost 350',
-      color: 'Cream V2'
+      brand: 'Nike',
+      model: 'Air Max 2090',
+      color: 'White',
+      image: './Nike-Air-Max-2090-White.jpg'
     },
     {
-      brand: 'Adidas',
-      model: 'Yeezy Boost 350',
-      color: 'Cream V2'
-    },
-    {
-      brand: 'Adidas',
-      model: 'Yeezy Boost 350',
-      color: 'Cream V2'
+      brand: 'Puma',
+      model: 'RS-X',
+      color: 'White',
+      image: './Puma-RS-X-White.jpg'
     }
   ]
 
