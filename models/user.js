620187680e330d8039b5e3567f1128464d3aad6b
rcraig <ryanpcraig1@gmail.com>
2021-08-26 16:36:29 -0500

    Finished seed files

--- models/user.js
+++ models/user.js
@@ -4,8 +4,7 @@ const User = new Schema(
   {
     name: { type: String, required: true },
     age: { type: Number, required: true },
-    email: { type: String, required: true },
-    shoe_ids: { type: Schema.Types.ObjectId, ref: 'shoe_ids' }
+    email: { type: String, required: true }
   },
   { timestamps: true }
 )
--- seed/users.js
+++ seed/users.js
@@ -1,39 +1,29 @@
 const db = require('../db')
-const { Shoe, User } = require('../models')
+const { User } = require('../models')
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
-  const nike1 = await Shoe.find({ brand: 'Nike', model: 'Air Jordan' })
-  const addidaYeez = await Shoe.find({ brand: 'Adidas', model: 'Yeezy Boost 350 V2' })
-  const newBal = await Shoe.find({ brand: 'New Balance', model: '997' })
-  // const nmd = await Shoe.find({ brand: 'Adidas', model: 'NMD' })
-  // const nikeAirMax = await Shoe.find({ brand: 'Nike', model: 'Air Max 2090' })
-  // const pumaRS = await Shoe.find({ brand: 'Puma', model: 'RS-X' })
-
   const users = [
     {
       name: 'Bob Jones',
       age: 21,
-      email: 'BobJones1@Gmail.com',
-      shoe_ids: nike1[0]._id
+      email: 'BobJones1@Gmail.com'
     },
     {
       name: 'Ryan Craig',
       age: 18,
-      email: 'Rcraig@outlook.com',
-      shoe_ids: addidaYeez[0]._id
+      email: 'Rcraig@outlook.com'
     },
     {
       name: 'Nick Jonas',
       age: 22,
-      email: 'Nick@yahoo.com',
-      shoe_ids: newBal[0]._id
+      email: 'Nick@yahoo.com'
     }
   ]
 
   await User.insertMany(users)
-  console.log('Created users with posts!')
+  console.log('Created users!')
 }
 const run = async () => {
   await main()
