ce15f9868b55acc9d79b9964e085d68e0ec255e7
rcraig <ryanpcraig1@gmail.com>
2021-08-27 13:36:24 -0500

    Fixed database

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 // let MONGODB_URI = 'mongodb://127.0.0.1:27017/soleMatesDatabase'
-let MONGODB_URI = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/todo_tracker'
+let MONGODB_URI = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/soleMatesDatabase'
 
 mongoose
     .connect(MONGODB_URI, { 
